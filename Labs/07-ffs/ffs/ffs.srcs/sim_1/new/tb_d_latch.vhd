51b4e5b26a72a2661e44617eb2e084f151bd5956
xsisol01 <xsisol01@vutbr.cz>
2021-03-29 12:05:05 +0200

    2

--- Labs/07-ffs/ffs/ffs.srcs/sim_1/new/tb_d_latch.vhd
+++ Labs/07-ffs/ffs/ffs.srcs/sim_1/new/tb_d_latch.vhd
@@ -32,16 +32,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity tb_d_latch is
-Port(
+   
+ end tb_d_latch;
+
+architecture Behavioral of tb_d_latch is
+    
     signal s_en     : std_logic;
     signal s_arst   : std_logic;
     signal s_d      : std_logic;  
     signal s_q      : std_logic;
-    signal s_q_bar  : std_logic
-    );
-end tb_d_latch;
-
-architecture Behavioral of tb_d_latch is
+    signal s_q_bar  : std_logic;
 
 begin
     uut_d_latch : entity work.d_latch
@@ -54,20 +54,106 @@ begin
     );
          
          
-    p_reset_gen : process
-    begin
-        s_arst <= '0';    
-    
-    end process p_reset_gen;
-    
-    p_stimulus : process
-    begin
-        report "stimulus process start" severity note;
-        s_d  <= '0';
-        s_en <= '0';
-        
-    
-    end process p_stimulus;
+    --------------------------------------------------------------------
+-- Reset generation process
+--------------------------------------------------------------------
+ p_reset_gen : process
+ begin
+	 s_arst <= '0';
+	 wait for 38 ns;
+	 
+	 -- Reset activated
+	 s_arst <= '1';
+	 wait for 53 ns;
+
+	 --Reset deactivated
+	 s_arst <= '0';
+	
+	 wait for 80 ns;
+	 s_arst <= '1';
+
+	 wait;
+ end process p_reset_gen;
+
+--------------------------------------------------------------------
+-- Data generation process
+--------------------------------------------------------------------
+p_stimulus : process
+begin
+	report "Stimulus process started" severity note;
+	
+	s_d  <= '0';
+	s_en <= '0';
+	wait for 10 ns;
+	
+	--remember/hold values (no value to hold)
+	s_d  <= '1';
+	wait for 10 ns;
+	s_d  <= '0';
+	wait for 10 ns;
+	s_d  <= '1';
+	--reset set to 1 -> all values '0' except q_bar
+	wait for 10 ns;
+	s_d  <= '0';
+	wait for 10 ns;
+	s_d  <= '1';
+	wait for 10 ns;
+
+	--Reseting output q
+	s_d  <= '0';
+	s_en <= '1';
+	assert ((s_arst = '0') and (s_en = '1'))
+	report "s_en setted to one -> Reseting output q" severity note;	
+	wait for 10 ns;
+	
+	--Seting output q - en is setted to 1
+	s_d  <= '1';
+	assert ((s_arst = '0') and (s_en = '1'))
+	report "s_en setted to one -> Seting output q" severity note;	
+	wait for 10 ns;
+	
+	
+	s_d  <= '0';
+	wait for 10 ns;   
+	s_d  <= '1';
+	wait for 10 ns;
+	s_d  <= '0';
+	wait for 10 ns;
+	s_d  <= '1';
+	--reset set to 0 - again operating 
+	wait for 10 ns;
+	s_d  <= '0';
+	wait for 10 ns;
+	s_d  <= '1';
+	wait for 10 ns;
+
+
+	-- Remember/hold values 
+	s_en <= '0';
+	assert ((s_arst = '0') and (s_en = '0'))
+	report "s_en setted to zero -> remember/hold value" severity note;
+	wait for 10 ns;
+	
+	s_d  <= '1';
+	wait for 10 ns;
+	s_d  <= '0';
+	wait for 10 ns;
+	s_d  <= '1';
+	wait for 10 ns;
+	s_d  <= '0';
+	wait for 10 ns;
+	s_d  <= '1';
+	--reset set to 1 - all values should be '0'
+	wait for 10 ns;
+	s_d  <= '0';
+	wait for 10 ns;
+	s_d  <= '1';
+	wait for 10 ns;
+	s_d  <= '0';
+	
+	report "Stimulus process finished" severity note;
+	wait;
+end process p_stimulus;
     
        
 
